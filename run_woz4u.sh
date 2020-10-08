2530318a55c657740caaa95d47da08bf3e62e128
Finn Rietz <5rietz@informatik.uni-hamburg.de>
2020-10-08 10:27:47 +0200

    Add easy start script

--- run_woz4u.sh
+++ run_woz4u.sh
@@ -1,2 +1,9 @@
-export PYTHONPATH=${PYTHONPATH}:/Users/finn/Desktop/WTM/pepper_scripts/pynaoqi-python2.7-2.5.7.1-mac64/lib/python2.7/site-packages
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/Users/finn/Desktop/WTM/pepper_scripts/pynaoqi-python2.7-2.5.7.1-mac64/lib
+abs_naoqi_path=/Users/finn/Desktop/WTM/pepper_scripts/pynaoqi-python2.7-2.5.7.1-mac64/lib/python2.7/site-packages
+abs_naoqi_lib_path=/Users/finn/Desktop/WTM/pepper_scripts/pynaoqi-python2.7-2.5.7.1-mac64/lib
+abs_venv_activate_path=/Users/finn/Desktop/Umea_project/venv/bin/activate
+
+export PYTHONPATH=${PYTHONPATH}:$abs_naoqi_path
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:$abs_naoqi_lib_path
+source $abs_venv_activate_path
+
+python server.py
