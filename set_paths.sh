56ea6b7c2847b4a0ccb631b7554ab09944adb1ba
Shambhawi <shambhawipal.official@gmail.com>
2023-03-16 18:32:03 +0100

    forked for group project

--- set_paths.sh
+++ set_paths.sh
@@ -1,12 +1,14 @@
 if [[ "$OSTYPE" == "msys" ]]; then
   # set env variables on windows, assuming GitBash shell
   echo Setting environment variables for GitBash on Windows:
-  PYTHONPATH="C:\Users\Finn\Desktop\WoZ4U\pynaoqi-python2.7-2.5.7.1-win32-vs2013\lib";
+  #set your path here
+  PYTHONPATH="C:\Users\shamb\pynaoqi-python2.7-2.5.7.1-win32-vs2013\lib";
   export PYTHONPATH
   echo $PYTHONPATH
 else
   # set env variables on linux/mac
   echo Setting environment variables for Bash on Linux/Mac
+  #set your path here
   export PYTHONPATH=${PYTHONPATH}:/Users/finn/Desktop/WTM/pepper_scripts/pynaoqi-python2.7-2.5.7.1-mac64/lib/python2.7/site-packages
   export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/Users/finn/Desktop/WTM/pepper_scripts/pynaoqi-python2.7-2.5.7.1-mac64/lib
 fi
