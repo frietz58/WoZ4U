45a6a8bcf064ea9a4068823e517fac1c7b6a0a94
Shambhawi <shambhawipal.official@gmail.com>
2023-03-28 12:58:33 +0200

    fixed sentences

--- set_paths.sh
+++ set_paths.sh
@@ -2,8 +2,8 @@ if [[ "$OSTYPE" == "msys" ]]; then
   # set env variables on windows, assuming GitBash shell
   echo Setting environment variables for GitBash on Windows:
   #set your path here
-  #PYTHONPATH="C:\Users\shamb\pynaoqi-python2.7-2.5.7.1-win32-vs2013\lib";
-  PYTHONPATH="C:\Users\deepa\anaconda3\envs\sce_env\Lib\site-packages\naoqi\lib";
+  PYTHONPATH="C:\Users\shamb\pynaoqi-python2.7-2.5.7.1-win32-vs2013\lib";
+  #PYTHONPATH="C:\Users\deepa\anaconda3\envs\sce_env\Lib\site-packages\naoqi\lib";
 
   export PYTHONPATH
   echo $PYTHONPATH
